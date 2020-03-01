0a6d98560243fb8fbbac490e1c5551aefa2172a6
Walmir Sa <walmir.sa@gmial.com>
2020-03-01 20:09:43 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _InfoText = "Pode Entrar";
+
+  void _changepeole(int delta) {
+    setState(() {
+      _people += delta;
+      if (_people < 0) {
+        _InfoText = "Mundo Invertido!";
+      } else if (_people <= 10) {
+        _InfoText = "Pode entrar";
+      } else {
+        _InfoText = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "imagens/original.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: $_people",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changepeole(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changepeole(-1);
+                    },
+                  ),
+                )
+              ],
+            ),
+            Text(_InfoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0))
+          ],
+        )
+      ],
+    );
+  }
+}
